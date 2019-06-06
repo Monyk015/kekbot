6d46c03b97908bdcaad40029e5520450699282ae
Constantine Manakov <kostiantyn.manakov@edetek.com>
2019-06-06 11:09:14 +0300

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -82,3 +82,4 @@ config :kekbot, Kekbot.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
+
