2b68d9e79f2fb32026003cc5d34f55b4612edc79
Constantine Manakov <kostiantyn.manakov@edetek.com>
2019-06-06 13:08:42 +0300

    conf

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,6 @@ use Mix.Config
 config :kekbot, Kekbot.Endpoint,
   url: [scheme: "https", host: "kekbot015", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
